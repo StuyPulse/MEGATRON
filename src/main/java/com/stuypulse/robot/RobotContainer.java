b8faae2cb77a51205a94ed5a2dd34152bb386b32
Kalimul Kaif <kalimul.kaif@stuypulse.com>
2024-07-10 18:54:54 -0400

    Added button bindings to test intake and shooter

--- src/main/java/com/stuypulse/robot/RobotContainer.java
+++ src/main/java/com/stuypulse/robot/RobotContainer.java
@@ -1,6 +1,15 @@
 package com.stuypulse.robot;
 
+import com.stuypulse.robot.commands.arm.ArmToFeed;
+import com.stuypulse.robot.commands.arm.ArmToLobFerry;
+import com.stuypulse.robot.commands.arm.ArmToLowFerry;
+import com.stuypulse.robot.commands.arm.ArmToSpeaker;
 import com.stuypulse.robot.commands.auton.DoNothingAuton;
+import com.stuypulse.robot.commands.intake.IntakeAcquire;
+import com.stuypulse.robot.commands.intake.IntakeStop;
+import com.stuypulse.robot.commands.shooter.ShooterAcquireFromIntake;
+import com.stuypulse.robot.commands.shooter.ShooterFerry;
+import com.stuypulse.robot.commands.shooter.ShooterScoreSpeaker;
 import com.stuypulse.robot.commands.swerve.SwerveDriveDrive;
 import com.stuypulse.robot.constants.Ports;
 import com.stuypulse.stuylib.input.Gamepad;
@@ -56,7 +65,29 @@ public class RobotContainer {
     /*** BUTTONS ***/
     /***************/
 
-    private void configureButtonBindings() {}
+    private void configureButtonBindings() {
+        driver.getLeftTriggerButton()
+            .onTrue(new IntakeAcquire())
+            .onFalse(new IntakeStop());
+        
+        driver.getRightTriggerButton()
+            .onTrue(new ArmToFeed()
+                .andThen(new ShooterAcquireFromIntake())
+                .andThen(new ArmToSpeaker())
+                .andThen(new ShooterScoreSpeaker()));
+        
+        driver.getTopButton()
+            .onTrue(new ArmToFeed()
+                .andThen(new ShooterAcquireFromIntake())
+                .andThen(new ArmToLobFerry())
+                .andThen(new ShooterFerry()));
+
+        driver.getBottomButton()
+            .onTrue(new ArmToFeed()
+                .andThen(new ShooterAcquireFromIntake())
+                .andThen(new ArmToLowFerry())
+                .andThen(new ShooterFerry()));
+    }
 
     /**************/
     /*** AUTONS ***/
