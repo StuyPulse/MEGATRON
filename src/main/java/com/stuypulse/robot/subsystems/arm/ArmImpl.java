80fe8d7e8188c6398439879959389e0532fa3786
Ian Shi <Ianshi1026@gmail.com>
2024-08-27 01:26:25 -0400

    fix logging

--- src/main/java/com/stuypulse/robot/subsystems/arm/ArmImpl.java
+++ src/main/java/com/stuypulse/robot/subsystems/arm/ArmImpl.java
@@ -71,9 +71,7 @@ public class ArmImpl extends Arm {
         if (state == State.FEED) {
             return atValidFeedAngle();
         }
-        boolean atTarget = Math.abs(getTargetDegrees() - getDegrees()) < Settings.Arm.MAX_ANGLE_ERROR.get();
-        SmartDashboard.putBoolean("test/armAtTarget", atTarget);
-        return atTarget;
+        return Math.abs(getTargetDegrees() - getDegrees()) < Settings.Arm.MAX_ANGLE_ERROR.get();
     }
 
     @Override
@@ -211,6 +209,8 @@ public class ArmImpl extends Arm {
         SmartDashboard.putNumber("Arm/Right Duty Cycle", rightMotor.get());
 
         SmartDashboard.putNumber("Arm/Arm Angle", getDegrees());
-        SmartDashboard.putNumber("Arm/Shooter Angle", getDegrees() + 96); // shooter is offset 96 degrees counterclockwise from arm (thanks kevin)
+        SmartDashboard.putNumber("Arm/Shooter Angle", getDegrees() + 96); // shooter is offset 96 degrees counterclockwise from arm (thanks kevin)]
+
+        SmartDashboard.putBoolean("Arm/At Target", atTarget());
     }
 }
