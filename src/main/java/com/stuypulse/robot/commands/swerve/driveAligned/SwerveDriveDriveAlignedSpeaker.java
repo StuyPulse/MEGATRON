505426bc0e1617b252d8559d4853bfd2385f8271
Ian Shi <Ianshi1026@gmail.com>
2024-08-27 22:19:41 -0400

    fix getAmpCornerPose()

--- src/main/java/com/stuypulse/robot/commands/swerve/driveAligned/SwerveDriveDriveAlignedSpeaker.java
+++ src/main/java/com/stuypulse/robot/commands/swerve/driveAligned/SwerveDriveDriveAlignedSpeaker.java
@@ -4,11 +4,8 @@ import com.stuypulse.robot.constants.Field;
 import com.stuypulse.robot.subsystems.swerve.SwerveDrive;
 import com.stuypulse.stuylib.input.Gamepad;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.smartdashboard.FieldObject2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SwerveDriveDriveAlignedSpeaker extends SwerveDriveDriveAligned{
     
@@ -20,8 +17,6 @@ public class SwerveDriveDriveAlignedSpeaker extends SwerveDriveDriveAligned{
     protected Rotation2d getTargetAngle() {
         Translation2d currentPose = SwerveDrive.getInstance().getPose().getTranslation();
         Translation2d speakerPose = Field.getAllianceSpeakerPose().getTranslation();
-        SmartDashboard.putNumber("test/speakerPoseX", speakerPose.getX());
-        SmartDashboard.putNumber("test/speakerPoseY", speakerPose.getY());
         return currentPose.minus(speakerPose).getAngle();
     }
 
