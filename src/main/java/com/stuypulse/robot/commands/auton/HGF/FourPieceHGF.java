ec61bdf1d208002244e0e80ddae1e98dab36e913
jopy-man <jophyw2008@gmail.com>
2024-09-12 19:07:32 -0400

    made red hgf, red adef, added waypoints for hgf adef and bca, removed one piece autons,.

--- src/main/java/com/stuypulse/robot/commands/auton/HGF/FourPieceHGF.java
+++ src/main/java/com/stuypulse/robot/commands/auton/HGF/FourPieceHGF.java
@@ -2,6 +2,7 @@ package com.stuypulse.robot.commands.auton.HGF;
 
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.stuypulse.robot.commands.arm.ArmToFeed;
+import com.stuypulse.robot.commands.auton.FollowPathThenShoot;
 import com.stuypulse.robot.commands.auton.ShootRoutine;
 import com.stuypulse.robot.commands.intake.IntakeSetAcquire;
 import com.stuypulse.robot.commands.swerve.SwerveDriveToPose;
@@ -18,31 +19,25 @@ public class FourPieceHGF extends SequentialCommandGroup {
      public FourPieceHGF(PathPlannerPath... paths) {
         addCommands(
             // Preload Shot
-            ShootRoutine.fromSubwoofer(),
+            ShootRoutine.fromAnywhere(),
             new ArmToFeed(),
 
             // Drive to H + Shoot H
             new IntakeSetAcquire(),
             SwerveDrive.getInstance().followPathCommand(paths[0]),
-            new WaitCommand(1.0).until(() -> Shooter.getInstance().hasNote()),
-            SwerveDrive.getInstance().followPathWithSpeakerAlignCommand(paths[1]),
-            ShootRoutine.fromAnywhere().withTimeout(2.5).onlyIf(() -> Shooter.getInstance().hasNote()),
+            new FollowPathThenShoot(paths[1], false),
             new ArmToFeed(),
 
             // Drive to G + Shoot G
             new IntakeSetAcquire(),
             SwerveDrive.getInstance().followPathCommand(paths[2]),
-            new WaitCommand(1.0).until(() -> Shooter.getInstance().hasNote()),
-            SwerveDrive.getInstance().followPathWithSpeakerAlignCommand(paths[3]),
-            ShootRoutine.fromAnywhere().withTimeout(2.5).onlyIf(() -> Shooter.getInstance().hasNote()),
+            new FollowPathThenShoot(paths[3], false),
             new ArmToFeed(),
 
             // Drive to F + Shoot F
             new IntakeSetAcquire(),
             SwerveDrive.getInstance().followPathCommand(paths[4]),
-            new WaitCommand(1.0).until(() -> Shooter.getInstance().hasNote()),
-            SwerveDrive.getInstance().followPathWithSpeakerAlignCommand(paths[5]),
-            ShootRoutine.fromAnywhere().withTimeout(2.5).onlyIf(() -> Shooter.getInstance().hasNote()),
+            new FollowPathThenShoot(paths[5], true),
             new ArmToFeed()
         );
     }
