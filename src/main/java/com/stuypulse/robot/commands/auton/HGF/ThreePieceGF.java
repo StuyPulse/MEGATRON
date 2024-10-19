75e79de1d81de6dedecff215986d8b25de24ff5f
jopy-man <jophyw2008@gmail.com>
2024-10-18 22:27:55 -0400

    Revert "stuff"
    This reverts commit e2674ee5ec79e43980e749c058bf5aaeedfd8f0b.

---
+++ src/main/java/com/stuypulse/robot/commands/auton/HGF/ThreePieceGF.java
@@ -0,0 +1,38 @@
+package com.stuypulse.robot.commands.auton.HGF;
+
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.stuypulse.robot.commands.arm.ArmToFeed;
+import com.stuypulse.robot.commands.auton.FollowPathThenShoot;
+import com.stuypulse.robot.commands.auton.ShootRoutine;
+import com.stuypulse.robot.commands.intake.IntakeSetAcquire;
+import com.stuypulse.robot.commands.swerve.SwerveDriveToPose;
+import com.stuypulse.robot.subsystems.shooter.Shooter;
+import com.stuypulse.robot.subsystems.swerve.SwerveDrive;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class ThreePieceGF extends SequentialCommandGroup {
+    
+     public ThreePieceGF(PathPlannerPath... paths) {
+        addCommands(
+            // Preload Shot
+            ShootRoutine.fromSubwoofer(),
+            new ArmToFeed(),
+
+            // Drive to G + Shoot G
+            new IntakeSetAcquire(),
+            SwerveDrive.getInstance().followPathCommand(paths[0]),
+            new FollowPathThenShoot(paths[1], false),
+            new ArmToFeed(),
+
+            // Drive to F + Shoot F
+            new IntakeSetAcquire(),
+            SwerveDrive.getInstance().followPathCommand(paths[2]),
+            new FollowPathThenShoot(paths[3], true),
+            new ArmToFeed()
+        );
+    }
+
+}
